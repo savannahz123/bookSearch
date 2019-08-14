c36a13be6890933460f11b92058af42635606bd3
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-08-13 23:23:06 -0700

    add navbar can now switch pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Search from "./pages/Search";
+import Save from "./pages/Save";
 import Navbar from "./Components/Navbar";
 
 class App extends Component {
@@ -7,21 +8,35 @@ class App extends Component {
         page: "search"
     }
 
+    onClickSearch = () => {
+        this.setState({page: "search"});
+    }
+
+    onClickSave = () => {
+        this.setState({page: "save"});
+    }
+
     render() {
         return (
             <div>
                 <Navbar>
-                    <a className="navbar-brand" href="#">My Personal Book App</a>
+                    <a className="navbar-brand" href="/">My Personal Book App</a>
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Search</a>
+                            <a className="nav-link" href="#" onClick={this.onClickSearch}>Search</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Saved</a>
+                            <a className="nav-link" href="#" onClick={this.onClickSave}>Saved</a>
                         </li>
                     </ul>
                 </Navbar>
-                <Search />
+                <div>
+                    {this.state.page === "search" ? (
+                        <Search />
+                    ) : (
+                        <Save />
+                    )}
+                </div>
             </div>
         )
     }
---
+++ client/src/pages/Save/Save.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class Save extends Component {
+    render() {
+        return (
+            <h1>Save!</h1>
+        );
+    }
+}
+
+export default Save;
